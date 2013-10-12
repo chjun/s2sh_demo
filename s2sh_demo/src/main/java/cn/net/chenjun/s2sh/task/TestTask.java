2b0a509e16fedc413652d2a87f18198c332ce1d8
chjun <chjun@163.com>
2013-10-12 09:57:42 +0800

    rename package

---
+++ s2sh_demo/src/main/java/cn/net/chenjun/s2sh/task/TestTask.java
@@ -0,0 +1,27 @@
+package cn.net.chenjun.s2sh.task;
+
+import java.util.Date;
+
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+
+import com.cernet.s2sh.dao.ArticleDao;
+
+public class TestTask extends QuartzJobBean {
+
+	@Override
+	protected void executeInternal(JobExecutionContext arg0)
+			throws JobExecutionException {
+		System.out.println("===========" + new Date()
+				+ ":task working ... ==============");
+		try {
+			Thread.sleep(3 * 1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("===========" + new Date()
+				+ ":task end. ==============");
+	}
+}
