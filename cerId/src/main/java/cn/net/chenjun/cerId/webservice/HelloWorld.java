6ca8fe14b2b68c057852aa63fc16705ebd372859
chjun <chjun3344@163.com>
2013-10-16 13:33:43 +0800

    cxf server

--- cerId/src/main/java/cn/net/chenjun/cerId/webservice/HelloWorld.java
+++ cerId/src/main/java/cn/net/chenjun/cerId/webservice/HelloWorld.java
@@ -2,7 +2,7 @@ package cn.net.chenjun.cerId.webservice;
 
 import javax.jws.WebService;
 
-@WebService
+@WebService()
 public interface HelloWorld {
-	public String sayHello(String foo);
+	String sayHello(String foo);
 }
--- cerId/src/main/java/cn/net/chenjun/cerId/webservice/impl/HelloWorldImpl.java
+++ cerId/src/main/java/cn/net/chenjun/cerId/webservice/impl/HelloWorldImpl.java
@@ -2,15 +2,21 @@ package cn.net.chenjun.cerId.webservice.impl;
 
 import javax.jws.WebService;
 
+import cn.net.chenjun.cerId.dao.PeopleBasicInfoDao;
+import cn.net.chenjun.cerId.model.PeopleBasicInfo;
 import cn.net.chenjun.cerId.webservice.HelloWorld;
 
-@WebService(serviceName="helloWorld")
+@WebService(serviceName = "helloworld", targetNamespace = "http://webservice.cerId.chenjun.net.cn/")
 public class HelloWorldImpl implements HelloWorld {
-	public HelloWorldImpl() {
-		System.out.println(123);
+
+	private PeopleBasicInfoDao peopleBasicInfoDao;
+
+	public void setPeopleBasicInfoDao(PeopleBasicInfoDao peopleBasicInfoDao) {
+		this.peopleBasicInfoDao = peopleBasicInfoDao;
 	}
 
 	public String sayHello(String foo) {
-		return "hello " + foo;
+		PeopleBasicInfo pbi = peopleBasicInfoDao.get(1L);
+		return "hello, foo! Is your cardNo " + pbi.getIdCardNo() + "?";
 	}
 }
